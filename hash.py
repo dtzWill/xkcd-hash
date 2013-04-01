073df836fc3b4b34e3115439f6e0f74bfa173fb6
Will Dietz <w@wdtz.org>
2013-04-01 13:06:12 -0500

    Much faster impl!

--- hash.py
+++ hash.py
@@ -5,22 +5,21 @@ from multiprocessing import Process, Value
 import string
 import random
 
-def hamming_distance(s1, s2):
-    assert len(s1) == len(s2)
-    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+from gmpy import hamdist
+
 
 best = Value('i',1000000000)
 
+goal = '5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6'
+int_goal=int(goal,16)
+
 def test(b):
-    goal = '5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6'
-
-    binary_goal = bin(int(goal, 16))[2:].zfill(1024)
-
     x=skein.skein1024(b)
     hex_x = x.hexdigest()
-    binary = bin(int(hex_x,16))[2:].zfill(1024)
-
-    dist = hamming_distance(binary, binary_goal)
+    int_x = int(hex_x,16)
+    dist = hamdist(int_x, int_goal)
 
     if dist < best.value:
         best.value = dist
@@ -33,10 +32,13 @@ def test(b):
         print(repr(b))
         quit()
 
-import itertools
 
 def randstr():
   return '%040x' % random.randrange(256**20)
+
+def testiter():
+    t = randstr()
+    test(bytes(t,'ascii'))
 
 class Thread(Process):
     def __init__(self):
@@ -44,8 +46,8 @@ class Thread(Process):
 
     def run(self):
         while True:
-            t = randstr()
-            test(bytes(t,'ascii'))
+            testiter()
+
 
 for i in range(16):
     f = Thread()
